8097e2ab1fd93742b0d65cff358545998c2a0822
Mirandasigalingging <163814351+Mirandasigalingging@users.noreply.github.com>
2025-05-17 15:33:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
+import pandas as pd
 from utils.utils_extract import extract_page
 from utils.utils_transform import transform_data
 from utils.utils_load import save_csv, save_to_google_sheets
 
-# kode main seperti biasa
-
 def main():
     all_products = []
-    total_pages = 50
-    for page in range(1, total_pages + 1):
+    for page in range(1, 51):
+        print(f"Extract halaman {page}")
         products = extract_page(page)
-        all_products.extend(products)
         print(f"Extracted page {page}, produk: {len(products)}")
-    
+        all_products.extend(products)
+
     df = pd.DataFrame(all_products)
-    print("Kolom DataFrame:", df.columns)
+    print(f"Total produk: {len(df)}")
     print("Contoh data:\n", df.head())
 
-    if df.empty:
-        print("DataFrame kosong, cek fungsi extract_page()")
-        return
-
+    # proses transform
     df_clean = transform_data(df)
-    save_csv(df_clean, 'products.csv')
-    print("Data disimpan ke CSV.")
 
-    creds_json = 'tough-anagram-460012-t4-e5a2928ac1b3.json'  # Sesuaikan path file creds
-    sheet_url = 'https://docs.google.com/spreadsheets/d/1hTQoQgDOx0By5-C6G8hK3qofpUK1qfCYU__F1BW1DQc/edit?usp=sharing'
-    save_to_google_sheets(df_clean, creds_json, sheet_url)
-    print("Data berhasil disimpan ke Google Sheets.")
+    # simpan CSV
+    save_csv(df_clean, 'products.csv')
+
+    # simpan ke Google Sheets
+    creds_path = 'google-sheets-api.json'
+    sheet_url = 'https://docs.google.com/spreadsheets/d/ID_SHEET_YANG_DIBAGI/edit?usp=sharing' # ganti sesuai
+    save_to_google_sheets(df_clean, creds_path, sheet_url)
 
 if __name__ == "__main__":
     main()
