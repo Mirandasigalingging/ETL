893bac7817c25d61466e705ef71a166076fbeb15
Mirandasigalingging <163814351+Mirandasigalingging@users.noreply.github.com>
2025-05-19 12:06:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from utils.extract import fetch_products
 from utils.transform import process_data
 from utils.load import save_to_csv, save_to_google_sheets
+import sys
+sys.path.append('/content/ETL')  # sesuaikan path ke folder utama repo
 
 def main():
     raw_data = fetch_products(pages=50)
