31b3c5d951c8d1b56f0cddf9402fee658cbf77fb
Mirandasigalingging <163814351+Mirandasigalingging@users.noreply.github.com>
2025-05-19 13:02:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from utils.utils_extract import fetch_products
 from utils.utils_transform import process_data
 from utils.utils_load import save_to_csv, save_to_google_sheets
-import sys
-sys.path.append('/content/ETL')  # sesuaikan path ke folder utama repo
+
 
 def main():
     raw_data = fetch_products(pages=50)
     df = process_data(raw_data)
     save_to_csv(df)
-    save_to_google_sheets(df, json_keyfile='etl-fashion-data-e7a102c009e2.json')
+    save_to_google_sheets(df, json_keyfile='etl-fashion-data-ee489a18d56a.json')
 
 if __name__ == "__main__":
     main()
