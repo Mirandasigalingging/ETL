7179f25430d42c118ea42cb7473e0de0f2f42de7
Mirandasigalingging <163814351+Mirandasigalingging@users.noreply.github.com>
2025-05-19 18:06:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from utils.utils_transform import process_data, clean_data
 from utils.utils_load import save_to_csv, save_to_google_sheets
 
 def main():
-    # Ambil data dari proses extract
     raw_data = fetch_products(pages=50)
-    
-    # Transform data awal
     df = process_data(raw_data)
-    
-    # Bersihkan data agar sesuai kriteria
+
+    # Cek nama-nama kolom
+    print("Kolom data:", df.columns.tolist())
+
+    # Pastikan kolom yang benar sudah sesuai
     df_bersih = clean_data(df)
-    
-    # Debug: lihat data bersih sebelum disimpan
+
+    # Cek data bersih
     print("Data bersih:")
     print(df_bersih.head())
     print("Jumlah baris:", len(df_bersih))
     
-    # Simpan ke CSV dan Google Sheets
+    # Simpan hasil bersih
     save_to_csv(df_bersih)
     save_to_google_sheets(df_bersih, json_keyfile='etl-fashion-data-ee489a18d56a.json')
 
